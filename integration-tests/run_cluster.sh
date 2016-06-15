1d54b2c6f0bfca084fe9180206de19c9d464e9bf
Charles Allen <charles@allen-net.com>
2016-06-15 13:22:56 -0700

    Stricter integration test script compliance

--- integration-tests/run_cluster.sh
+++ integration-tests/run_cluster.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -eux
+
 # cleanup 
 for node in druid-historical druid-coordinator druid-overlord druid-router druid-broker druid-middlemanager druid-zookeeper-kafka druid-metadata-storage;
 do
