a08b877591828d5d70be3c2aeef3f27b59e5cd6f
leventov <leventov@ya.ru>
2016-10-06 17:55:33 +0300

    Fix FilteredAggregatorFactory.toString()

--- processing/src/main/java/io/druid/query/aggregation/FilteredAggregatorFactory.java
+++ processing/src/main/java/io/druid/query/aggregation/FilteredAggregatorFactory.java
@@ -180,7 +180,7 @@ public class FilteredAggregatorFactory extends AggregatorFactory
   public String toString()
   {
     return "FilteredAggregatorFactory{" +
-           ", delegate=" + delegate +
+           "delegate=" + delegate +
            ", filter=" + filter +
            '}';
   }
