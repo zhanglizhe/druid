b49126195a020ac1ac88bc1a2c9796804899be6a
dgolitsyn <golitsyn.dima@gmail.com>
2017-02-16 01:26:52 +0300

    fix task predicates in twoCloudWorkerProvisioningStrategy

--- indexing-service/src/main/java/io/druid/indexing/overlord/autoscaling/TwoCloudWorkerProvisioningStrategy.java
+++ indexing-service/src/main/java/io/druid/indexing/overlord/autoscaling/TwoCloudWorkerProvisioningStrategy.java
@@ -19,16 +19,21 @@
 
 package io.druid.indexing.overlord.autoscaling;
 
+import com.google.common.base.Predicate;
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.inject.Inject;
 import com.metamx.common.concurrent.ScheduledExecutors;
+import io.druid.indexing.common.task.Task;
 import io.druid.indexing.overlord.TasksAndWorkers;
 import io.druid.indexing.overlord.WorkerTaskRunner;
 import io.druid.indexing.overlord.setup.TwoCloudConfig;
 import io.druid.indexing.overlord.setup.WorkerBehaviorConfig;
 
+import javax.annotation.Nullable;
 import java.util.concurrent.ScheduledExecutorService;
+
+import static io.druid.indexing.common.task.TaskLabels.getTaskLabel;
 
 public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioningStrategy
 {
@@ -41,8 +46,10 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
     }
   };
 
+  private final String taskLabel1;
   private final String ipPrefix1;
   private final PendingTaskBasedWorkerProvisioningStrategy provisioningStrategy1;
+  private final String taskLabel2;
   private final String ipPrefix2;
   private final PendingTaskBasedWorkerProvisioningStrategy provisioningStrategy2;
 
@@ -54,8 +61,10 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
   )
   {
     this(
+        twoCloudConfig.get().getTaskLabel1(),
         twoCloudConfig.get().getIpPrefix1(),
         twoCloudConfig.get().getWorkerBehaviorConfig1(),
+        twoCloudConfig.get().getTaskLabel2(),
         twoCloudConfig.get().getIpPrefix2(),
         twoCloudConfig.get().getWorkerBehaviorConfig2(),
         config,
@@ -64,8 +73,10 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
   }
 
   public TwoCloudWorkerProvisioningStrategy(
+      String taskLabel1,
       String ipPrefix1,
       WorkerBehaviorConfig workerBehaviorConfig1,
+      String taskLabel2,
       String ipPrefix2,
       WorkerBehaviorConfig workerBehaviorConfig2,
       PendingTaskBasedWorkerProvisioningConfig config,
@@ -73,6 +84,8 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
   )
   {
     super(provisioningSchedulerConfig, EXEC_FACTORY);
+    this.taskLabel1 = taskLabel1;
+    this.taskLabel2 = taskLabel2;
     this.ipPrefix1 = ipPrefix1;
     this.ipPrefix2 = ipPrefix2;
     provisioningStrategy1 = new PendingTaskBasedWorkerProvisioningStrategy(
@@ -94,11 +107,14 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
   @Override
   Provisioner makeProvisioner(TasksAndWorkers runner)
   {
+    final TaskPredicate taskPredicate1 = new TaskPredicate(taskLabel1, true);
+    final TaskPredicate taskPredicate2 = new TaskPredicate(taskLabel2, false);
+
     final Provisioner provisioner1 = provisioningStrategy1.makeProvisioner(
-        new TasksAndWorkersFilteredByIp((WorkerTaskRunner) runner, ipPrefix1)
+        new TasksAndWorkersFilteredByIp((WorkerTaskRunner) runner, ipPrefix1, taskPredicate1)
     );
     final Provisioner provisioner2 = provisioningStrategy2.makeProvisioner(
-        new TasksAndWorkersFilteredByIp((WorkerTaskRunner) runner, ipPrefix2)
+        new TasksAndWorkersFilteredByIp((WorkerTaskRunner) runner, ipPrefix2, taskPredicate2)
     );
     return new Provisioner()
     {
@@ -121,4 +137,25 @@ public class TwoCloudWorkerProvisioningStrategy extends AbstractWorkerProvisioni
       }
     };
   }
+
+  private class TaskPredicate implements Predicate<Task> {
+    private final String taskLabel;
+    private final boolean acceptNullLabel;
+
+    public TaskPredicate(String taskLabel, boolean acceptNullLabel) {
+      this.taskLabel = taskLabel;
+      this.acceptNullLabel = acceptNullLabel;
+    }
+
+    @Override
+    public boolean apply(@Nullable Task task)
+    {
+      if (task == null) {
+        return false;
+      }
+      String label = getTaskLabel(task);
+      return label == null ? acceptNullLabel : label.equals(taskLabel);
+    }
+  }
+
 }
