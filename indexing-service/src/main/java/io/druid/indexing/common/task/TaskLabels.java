975e1ff0a955ffeab6a9faf1ea0dd96a2ee8295e
dmitry.golitsyn <golitsyn.dima@gmail.com>
2017-03-09 16:01:43 +0300

    Fix NPE in TaskLabels

--- indexing-service/src/main/java/io/druid/indexing/common/task/TaskLabels.java
+++ indexing-service/src/main/java/io/druid/indexing/common/task/TaskLabels.java
@@ -22,13 +22,16 @@ package io.druid.indexing.common.task;
 
 import com.google.common.base.Preconditions;
 
+import java.util.Map;
+
 public class TaskLabels
 {
   public static final String TASK_LABEL_FIELD = "label";
 
   public static String getTaskLabel(Task task) {
     Preconditions.checkNotNull(task, "task");
-    Object taskLabel = task.getContext().get(TASK_LABEL_FIELD);
+    Map<String, Object> context = task.getContext();
+    Object taskLabel = (context != null) ? context.get(TASK_LABEL_FIELD) : null;
     return taskLabel == null ? null : (String) taskLabel;
   }
 }
